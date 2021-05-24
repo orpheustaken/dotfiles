9d55878daf13b6ebfa47aea71e617a1ee5b172d5
Matheus Vinicius <matheusvinicius7000@gmail.com>
2021-05-23 21:28:45 -0300

    tabs

--- dwm/config.h
+++ dwm/config.h
@@ -1,11 +1,11 @@
 #include <X11/XF86keysym.h>
 
-// volume keys
+// volume control
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 
-// brightness keys
+// brightness control
 static const char *brightnessup[] = { "sudo", "xbacklight", "-inc", "5", NULL };
 static const char *brightnessdown[] = { "sudo", "xbacklight", "-dec", "5", NULL };
 
