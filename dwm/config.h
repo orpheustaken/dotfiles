282a52c5639bd26b141370545191fd461af13be7
Matheus Vinicius <matheusvinicius7000@gmail.com>
2021-06-05 17:49:45 -0300

    increase gaps to 12

--- dwm/config.h
+++ dwm/config.h
@@ -11,7 +11,7 @@ static const char *brightnessdown[] = { "sudo", "xbacklight", "-dec", "5", NULL 
 
 /* appearance */
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 12;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
