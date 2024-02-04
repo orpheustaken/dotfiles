32bc886d97ed6901b9387c1b630f5c9b2708fcc9
Matheus Fernandes <matheusvinicius7000@gmail.com>
2024-02-03 21:53:59 -0300

    Vim, Scripts: .sh file to automatically setup vim

---
+++ scripts/setup-vim.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+mkdir $HOME/.vim
+
+mkdir $HOME/.vim/pack
+mkdir $HOME/.vim/undodir
+
+mkdir $HOME/.vim/pack/plugins
+mkdir $HOME/.vim/pack/plugins/start
+mkdir $HOME/.vim/pack/themes
+mkdir $HOME/.vim/pack/themes/start
+
+cd $HOME/.vim/pack/plugins/start
+
+git clone --depth 1 git@github.com:preservim/nerdtree.git
+git clone --depth 1 git@github.com:airblade/vim-gitgutter.git
+git clone --depth 1 git@github.com:sheerun/vim-polyglot.git
+
+cd $HOME/.vim/pack/themes/start
+
+git clone --depth 1 git@github.com:joshdick/onedark.vim.git
+mv onedark.vim/ onedark/
+
+cd $HOME
+
+cp $HOME/Projects/Git/dotfiles/vim/vim_files/vimrc .
+mv vimrc .vimrc
